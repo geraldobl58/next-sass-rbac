0f479bc88d6b13691abb7543ae7d0147b2bb75e6
Geraldo Luiz <geraldobl58@gmail.com>
2025-07-15 09:27:29 -0300

    feat: integrate Zod for schema validation and enhance role-based permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,20 +1,22 @@
 import { AbilityBuilder } from '@casl/ability'
 import { User } from './models/user'
+import { Roles } from './roles'
 import { AppAbility } from '.'
-
-type Role = 'ADMIN' | 'MEMBER'
 
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>
 ) => void
 
-export const permissions: Record<Role, PermissionsByRole> = {
+export const permissions: Record<Roles, PermissionsByRole> = {
   ADMIN(_, { can }) {
     can('manage', 'all') // Admins can manage everything
   },
   MEMBER(_, { can }) {
-    can('invite', 'User') // Members can invite users
+    // can('invite', 'User') // Members can invite users
     can('manage', 'Project') // Members can manage projects
+  },
+  BILLING(_, { can }) {
+    // can('manage', 'Billing') // Billing users can manage billing
   },
 }
