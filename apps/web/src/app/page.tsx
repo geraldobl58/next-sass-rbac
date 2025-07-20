58723ff3696a7d548214d9c399bd29cf82f93b32
Geraldo Luiz <geraldobl58@gmail.com>
2025-07-20 11:57:46 -0300

    feat: add UI components and update styles with Tailwind CSS integration

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+
 export default function Home() {
   return (
     <div>
-      <div>Home</div>
+      <Label>Home</Label>
+      <Button>Home</Button>
+      <Input placeholder="Type something..." className="mt-2" />
     </div>
   )
 }
