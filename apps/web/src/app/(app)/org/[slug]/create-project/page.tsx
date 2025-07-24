ee5804968e32bc36b7f83bcd9c3cd89b50812899
Geraldo Luiz <geraldobl58@gmail.com>
2025-07-24 15:20:07 -0300

    feat: refactor layout and header components, add Tabs and NavLink for improved navigation

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,7 +1,5 @@
 import { ability } from '@/auth/auth'
 
-import { Header } from '@/components/header'
-
 import { ProjectForm } from './project-form'
 import { redirect } from 'next/navigation'
 
@@ -13,12 +11,9 @@ export default async function CreateProject() {
   }
 
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create Project</h1>
-        <ProjectForm />
-      </main>
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create Project</h1>
+      <ProjectForm />
     </div>
   )
 }
